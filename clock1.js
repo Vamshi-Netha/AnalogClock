1a0e117074934e7a14d12c814c92cc37a16cbc26
Vamshi-Netha <59342569+Vamshi-Netha@users.noreply.github.com>
2020-08-10 04:37:10 +0530

    Update clock1.js

--- clock1.js
+++ clock1.js
@@ -1,3 +1,4 @@
+const currentDate = new Date()
 const secondHand = document.getElementById("3")
 const minuteHand = document.querySelector('[data-minute-hand]')
 const hourHand = document.querySelector('[data-hour-hand]')
@@ -8,7 +9,8 @@ function setClock(){
     const hoursRatio = (minutesRatio+currentDate.getHours())/12
     setRotation(secondHand,secondsRatio)
     setRotation(minuteHand,minutesRatio)
-    setRotation(hourHand,hoursRatio)    
+    setRotation(hourHand,hoursRatio)  
+    
 }
 
 function setRotation(element, rotationRatio){
@@ -16,3 +18,35 @@ function setRotation(element, rotationRatio){
 }
 setInterval(setClock, 1000)
 setClock()
+/*  day month year*/
+let month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+let weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+let n = weekday[currentDate.getDay()];
+let m = month[currentDate.getMonth()];
+
+const date = currentDate.getDate()
+const year = currentDate.getFullYear()
+document.querySelector(".date1").innerHTML = date
+document.querySelector(".month").innerHTML = m
+document.querySelector(".year").innerHTML = year
+document.querySelector(".day").innerHTML = n
